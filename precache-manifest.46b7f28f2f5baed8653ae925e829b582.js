0550c8b5f4660844618abf5cfff114d659cebbab
velbharathi <velbharathi.a@zohocorp.com>
2018-12-28 15:36:20 +0530

    Updates

---
+++ precache-manifest.46b7f28f2f5baed8653ae925e829b582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "bc492cf32e756bd245a3",
+    "url": "/Portfolio/static/js/main.bc492cf3.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Portfolio/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "bc492cf32e756bd245a3",
+    "url": "/Portfolio/static/css/main.4656f2e9.chunk.css"
+  },
+  {
+    "revision": "4ef040f0a6456e554f4bc51c30649ab1",
+    "url": "/Portfolio/index.html"
+  }
+];
