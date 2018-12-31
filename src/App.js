1ad19c3c74bf14e367b952073ca199c90cb3b113
velbharathi <velbharathi.a@zohocorp.com>
2019-01-01 00:19:39 +0530

    background changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Hi, I'm Velbharathi.
-        </header>
+      <div className="container">
+        <div className="background">
+          <div className="cloud"></div>
+          <div className="moon"></div>
+          {/* <div className="star star1-animation"></div>
+          <div className="star star2-animation"></div>
+          <div className="star star3-animation"></div> */}
+        </div>
+        <div className="main-content">
+          <span className="intro">Hi hi, I'm <span>Velbharathi❤️</span>.<span className="blinking-cursor">_</span></span>
+          </div>
       </div>
     );
   }
